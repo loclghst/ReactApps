c2b20a88de0cca5fd176b451ec8b0a177be1699c
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-15 05:11:54 +0530

    memoize the action creator to prevent unnecessary network requests

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
 
 //fetchPost action creator
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,11 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-//fetchUser action creator
-export const fetchUser = id => async dispatch => {
+//memoizing the action creator
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
+
+//fetchUser action creator
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
