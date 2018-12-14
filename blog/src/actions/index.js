d29144dd5602ea4858d064910e64d7d6cff3875a
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-15 02:02:39 +0530

    only put required data on action payload

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 //this is the correct version
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
 //this is very wrong . it will give an error : Actions must be plain js objects Use custom middleware
