2600cbf0331835a5547672841de42c295bd6865c
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-15 02:34:43 +0530

    create the fetchUser action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,17 +1,13 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-//this is the correct version
+//fetchPost action creator
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-//this is very wrong . it will give an error : Actions must be plain js objects Use custom middleware
-//for async operations.
-// Although it seems that we are returning a js object but we are not, we are writing ES6 which
-// gets transpiled to ES5 for browsers, if we look into the transpile code we will see that because of
-// async-await we are not returning a js object, thats why the error
-
-// Another reason is that by the time our action gets to the reucer we wont have the fetched data
-
-//for correct code go to next commit
+//fetchUser action creator
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceHolder.get(`/user/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
