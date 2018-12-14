3570da35fc1995bbeeedd556133adaf750d66c48
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-14 14:00:15 +0530

    wire up redux-thunk to redux and fix the action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,12 +1,10 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: response
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
+
 //this is very wrong . it will give an error : Actions must be plain js objects Use custom middleware
 //for async operations.
 // Although it seems that we are returning a js object but we are not, we are writing ES6 which
@@ -14,6 +12,5 @@ export const fetchPosts = async () => {
 // async-await we are not returning a js object, thats why the error
 
 // Another reason is that by the time our action gets to the reucer we wont have the fetched data
-
 
 //for correct code go to next commit
