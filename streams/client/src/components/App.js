612bcc7294ab682013b13f95c04fd01cdcf398cf
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-17 02:33:14 +0530

    create the Header component and wire it up with the App component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const pageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const pageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={pageOne} />
-          <Route path="/2" component={pageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
         </div>
       </BrowserRouter>
     </div>
