c57dcade184ac5dcc26f5aade12adeda973fb8b2
Manish Kumar Sharma <loclghst@gmail.com>
2018-12-16 13:44:34 +0530

    alternate implementation of fetchPostsAndUsers

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,10 +5,19 @@ import _ from "lodash";
 
 export const fetchPostsAndUser = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  _.uniq(_.map(getState().posts, "userId")).forEach(id =>
-    dispatch(fetchUser(id))
-  );
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
+
+// export const fetchPostsAndUser = () => async (dispatch, getState) => {
+//   await dispatch(fetchPosts());
+//   _.uniq(_.map(getState().posts, "userId")).forEach(id =>
+//     dispatch(fetchUser(id))
+//   );
+// };
 
 //fetchPost action creator
 export const fetchPosts = () => async dispatch => {
